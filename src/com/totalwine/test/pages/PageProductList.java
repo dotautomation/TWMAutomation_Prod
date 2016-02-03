35e151b98ace02b4312a7ce1b3211001661cf17e
Rajat Sud <rajatsud@yahoo.com>
2016-02-03 16:32:15 -0500

    Search parameterization/top level menu/improvements

--- src/com/totalwine/test/pages/PageProductList.java
+++ src/com/totalwine/test/pages/PageProductList.java
@@ -1,7 +1,6 @@
 package com.totalwine.test.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class PageProductList {
