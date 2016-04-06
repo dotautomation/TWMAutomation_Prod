c032e5310f8f29ed6a7be195ea6136db2712dd3a
MdHTotalwine <mhossain@totalwine.com>
2016-04-06 10:40:22 -0400

    Update ConfigurationFunctions.java

--- src/com/totalwine/test/config/ConfigurationFunctions.java
+++ src/com/totalwine/test/config/ConfigurationFunctions.java
@@ -47,7 +47,7 @@ public class ConfigurationFunctions {
 	public static final String IEDRIVERPATH = "C:/twmautomation/lib/lib/IEDriverServer.exe";
 	public static final String RESULTSPATH = "C:\\autoreports\\";
 	public static final String TESTLOGIN = "rsud@live.com";
-	public static final String TESTPWD = "yoyo55";
+	public static final String TESTPWD = "grapes123";
 	//public static final WebDriver driver = new FirefoxDriver(testProfile);
 	
 	public static void DeleteCookies() {
